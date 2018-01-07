253cad910fc7ebab85d871292b7b038ea68f1f10
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-07 23:34:46 +0700

    Khoi tao pages sp ban chay va sp moi

---
+++ pages/pHome.php
@@ -0,0 +1,4 @@
+<?php
+    include ("pages/pSanPhamMoiNhat.php");
+    include ("pages/pSanPhamBanChayNhat.php");
+?>
---
+++ pages/pSanPhamBanChayNhat.php
@@ -0,0 +1,16 @@
+<h2>Sản Phẩm Bán Chạy Nhất</h2>
+<?php
+    $sql = "SELECT sp.MaSanPham, sp.TenSanPham, sp.GiaSanPham, sp.HinhURL
+            FROM SanPham sp
+            WHERE sp.BiXoa = 0
+            ORDER BY sp.SoLuongBan DESC
+            LIMIT 0, 10";
+        $list = DataProvider::ExecuteQuery($sql);
+        while($row = mysqli_fetch_array($list)){
+            $maSanPham = $row["MaSanPham"];
+            $tenSanPham = $row["TenSanPham"];
+            $giaSanPham = $row["GiaSanPham"];
+            $hinhURL = $row["HinhURL"];
+            include ("templates/tempThumbProduct.php");
+        }
+?>
