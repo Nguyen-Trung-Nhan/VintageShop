3a9f8cc764ab0f13f0748501a6f1f15d059819a3
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-07 21:26:09 +0700

    Khoi Tao index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/style.css" type="text/css">
+    <title>Vintage Shop</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header"> 
+            <?php include ('modules/mHeader.php'); ?>
+        </div>
+        <div class="sidebar">
+            <?php include ('modules/mMenuHang.php'); ?>
+            <?php include ('modules/mMenuLoai.php'); ?>   
+        </div>
+        <div class="content">
+            <?php include ('modules/mContent.php'); ?>
+        </div>
+        <div class="footer">
+                <?php include ('modules/mFooter.php'); ?>                
+        </div>
+    </div>
+</body>
+</html>
