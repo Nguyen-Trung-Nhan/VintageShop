060a030ed1b13d4cc805ae1d032e052df4ceb9c4
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-07 22:44:47 +0700

    Add slider & logo

---
+++ modules/mHeader.php
@@ -0,0 +1,13 @@
+<div class="header"> 
+    <a href="index.php">
+            <img src="img/logo.jpg" alt="image not found" class="logo">
+    </a>
+</div>
+<div class="sliderboxHead">
+    <div id="sliderbox" >
+        <img src="img/clock-slider1.jpg" alt="image not found">
+        <img src="img/clock-slider2.jpg" alt="image not found">
+        <img src="img/clock-slider3.jpg" alt="image not found">
+        <img src="img/clock-slider4.jpg" alt="image not found">
+    </div>
+</div>
