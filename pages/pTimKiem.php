eca01492b39ca46f7b57ac11598091541710f5fe
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-08 17:11:09 +0700

    Them trang tim kiem

---
+++ pages/pTimKiem.php
@@ -0,0 +1,20 @@
+<h2>Sản Phẩm Tìm Kiếm</h2>
+<?php
+    if(isset($_POST["txtTimKiem"]) && ($_POST["txtTimKiem"] != "")){
+    $timKiem = $_POST['txtTimKiem'];
+    $sql = "SELECT sp.MaSanPham, sp.TenSanPham, sp.GiaSanPham, sp.HinhURL
+            FROM SanPham sp 
+            WHERE sp.BiXoa = 0 and sp.TenSanPham like '%$timKiem%'" ;
+    $list = DataProvider::executeQuery($sql);
+    while($row = mysqli_fetch_array($list)){
+        $maSanPham = $row["MaSanPham"];
+        $tenSanPham = $row["TenSanPham"];
+        $giaSanPham = $row["GiaSanPham"];
+        $hinhURL = $row["HinhURL"];
+        include ("templates/tempThumbProduct.php");
+        }
+    }
+    else{
+        DataProvider::ChangeURL("index.php");
+    }
+?>
