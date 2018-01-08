80c9371b28fa0bb5288e37aee219575c56b47082
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-08 13:57:06 +0700

    Cap nhat phplogin vao mHeader

--- modules/mHeader.php
+++ modules/mHeader.php
@@ -1,15 +1,25 @@
-
+<div class="header">
     <a href="index.php">
             <img src="img/logo.jpg" alt="image not found" class="logo">
     </a>
     <table width="310" height="20" class="formTimKiem">
     <tr><td>
     <form action="index.php?a=16" method="POST" class="TimKiem" >
-        Tìm Kiếm:<input type="text" name="txtTimKiem" id="txtTimKiem">
+        Tìm kiếm:<input type="text" name="txtTimKiem" id="txtTimKiem">
         <button type="submit" name="btnTim">Tìm</button>
     </form>
     </td></tr>
     </table>
+    <?php
+        if(isset($_SESSION['maTaiKhoan']))
+        {
+            include ("modules/mThongTinTaiKhoan.php");
+        }
+        else
+        {
+            include ("modules/mDangNhapTaiKhoan.php");
+        }
+    ?>
 </div>
 <div class="sliderboxHead">
     <div id="sliderbox" >
@@ -18,3 +28,4 @@
         <img src="img/clock-slider3.jpg" alt="image not found">
         <img src="img/clock-slider4.jpg" alt="image not found">
     </div>
+</div>
