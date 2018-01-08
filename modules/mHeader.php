b7831634994af3c21ee57e1d4a04ea10b2de7517
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-08 12:01:10 +0700

    Lam form tim kiem

--- modules/mHeader.php
+++ modules/mHeader.php
@@ -2,6 +2,14 @@
     <a href="index.php">
             <img src="img/logo.jpg" alt="image not found" class="logo">
     </a>
+    <table width="310" height="20" class="formTimKiem">
+    <tr><td>
+    <form action="index.php?a=16" method="POST" class="TimKiem" >
+        Tìm Kiếm:<input type="text" name="txtTimKiem" id="txtTimKiem">
+        <button type="submit" name="btnTim">Tìm</button>
+    </form>
+    </td></tr>
+    </table>
 </div>
 <div class="sliderboxHead">
     <div id="sliderbox" >
