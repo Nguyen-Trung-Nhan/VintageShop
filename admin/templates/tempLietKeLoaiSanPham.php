9af9da5a82ca56ccc73f39a3ba280e7f01caca38
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-09 22:32:17 +0700

    cap nhat chi tiet cua san pham

---
+++ admin/templates/tempLietKeLoaiSanPham.php
@@ -0,0 +1,4 @@
+<tr>
+    <td><?php echo "$maLoaiSanPham"; ?></td>
+    <td><?php echo "$tenLoaiSanPham"; ?></td>
+</tr>
---
+++ admin/templates/tempLietKeSanPham.php
@@ -0,0 +1,12 @@
+<tr>
+<td><?php echo "$maSanPham"; ?></td>
+<td><?php echo "$tenSanPham"; ?></td>
+<td><?php echo "$giaSanPham"; ?></td>
+<td><img src="images/<?php echo "$hinhURL"; ?>" alt="" width="100" height="100"></td>
+<td><?php echo "$ngayNhap"; ?></td>
+<td><?php echo "$soLuongTon"; ?></td>
+<td><?php echo "$soLuongBan"; ?></td>
+<td><?php echo "$moTa"; ?></td>
+<td><?php echo "$maLoaiSanPham"; ?></td>
+<td><?php echo "$maHangSanXuat"; ?></td>
+</tr>
