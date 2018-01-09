3afe5a89f716e28f8e16e95b57c5e10f04101f85
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-09 15:03:55 +0700

    Them vao cau query lay maloaisanpham

--- pages/pChiTietCuaSanPham.php
+++ pages/pChiTietCuaSanPham.php
@@ -1,12 +1,13 @@
 <?php
     if(isset($_GET['id'])){
         $maSanPham = $_GET['id'];
-        $sql = "SELECT sp.MaSanPham, sp.TenSanPham, sp.GiaSanPham, sp.HinhURL, sp.SoLuongTon, sp.MoTa, lsp.TenLoaiSanPham, hsx.TenHangSanXuat
+        $sql = "SELECT sp.MaSanPham, sp.TenSanPham, sp.GiaSanPham, sp.HinhURL, sp.SoLuongTon, sp.MoTa, lsp.TenLoaiSanPham, hsx.TenHangSanXuat,lsp.MaLoaiSanPham
                 FROM SanPham sp, LoaiSanPham lsp, HangSanXuat hsx
                 WHERE sp.BiXoa = 0 AND sp.MaSanPham = $maSanPham AND sp.MaLoaiSanPham = lsp.MaLoaiSanPham AND sp.MaHangSanXuat = hsx.MaHangSanXuat";
         $list = DataProvider::ExecuteQuery($sql);
         while($row = mysqli_fetch_array($list)){
             $hinhURL = $row["HinhURL"];
+            $maLoaiSanPham = $row["MaLoaiSanPham"];
             $tenSanPham = $row["TenSanPham"];
             $giaSanPham = $row["GiaSanPham"];
             $tenHangSanXuat = $row["TenHangSanXuat"];
