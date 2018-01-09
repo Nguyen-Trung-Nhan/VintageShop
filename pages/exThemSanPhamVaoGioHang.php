2341ff6ff23c593d49f3f0547b650e2df0300b94
Nguyen-Trung-Nhan <nguyentrungnhan23897@gmail.com>
2018-01-09 18:27:24 +0700

    cap nhat dieu kien khi them vao gio hang

--- pages/exThemSanPhamVaoGioHang.php
+++ pages/exThemSanPhamVaoGioHang.php
@@ -2,13 +2,24 @@
     if(isset($_GET['id']))
     {
         $maSanPham = $_GET['id'];
+        $sql = "SELECT SoLuongTon
+        FROM sanpham
+        WHERE  MaSanPham = $maSanPham";
+        $list = DataProvider::ExecuteQuery($sql);
+        $row = mysqli_fetch_array($list);
+        $soLuongTon = $row["SoLuongTon"];
         $gioHang = new ShoppingCart();
         if(isset($_SESSION['gioHang']))
             $gioHang = unserialize($_SESSION['gioHang']);
-        $gioHang->Add($maSanPham);
-        $_SESSION['gioHang'] = serialize($gioHang);
+        if($soLuongTon > 0){
+            $gioHang->Add($maSanPham);
+            $_SESSION['gioHang'] = serialize($gioHang);
 
-        DataProvider::ChangeURL('index.php?a=6');
+            DataProvider::ChangeURL('index.php?a=6');
+        }
+        else{
+            DataProvider::ChangeURL("index.php?a=4&id=$maSanPham");
+        }
     }
     else
     {
