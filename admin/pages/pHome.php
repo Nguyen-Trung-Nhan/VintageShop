e50bf3f8b6a9315902138eb17890d4eb30361abc
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-09 22:03:20 +0700

    cap nhat phome vao trang admin

---
+++ admin/pages/pHome.php
@@ -0,0 +1,11 @@
+<div class="TieuDepHome">
+    <h1>Giao diện dành cho người quản lý</h1>
+</div>
+<span id="mota"><strong>Mô tả chức năng:</strong></span>
+<ul>
+    <li><strong>Quản lý sản phẩm: </strong> <span>cho phép thêm, xóa, sửa, liêt kê và tìm kiếm thông tin của sản phẩm.</span> </li>
+    <li><strong>Quản lý loại sản phẩm:</strong> <span> cho phép thêm, xóa, sửa, liệt kê và tìm kiếm thông tin của loại sản phẩm.</span></li>
+    <li><strong>Quản lý nhà sản xuất:</strong> <span> cho phép thêm, xóa, sửa, liệt kê và tìm kiếm thông tin của nhà sản xuất.</span></li>
+    <li><strong>Quản lý người dùng:</strong> <span> cho phép thêm, xóa, sửa, liệt kê và tìm kiếm thông tin tài khoản người dùng.</span></li>
+    <li><strong>Quản lý đơn hàng:</strong> <span> cho phép thêm, xóa, sửa, liệt kê và tìm kiếm thông tin đơn đặt hàng.</span> </li>
+</ul>
