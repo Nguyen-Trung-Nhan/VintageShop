344a7fe310a0d0bf3c8e192544251124e9afd8e7
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-09 23:17:17 +0700

    Tem templates nguoi dung

---
+++ admin/templates/tempLietKeNguoiDung.php
@@ -0,0 +1,10 @@
+<tr>
+    <td><?php echo "$maTaiKhoan"; ?></td>
+    <td><?php echo "$tenDangNhap"; ?></td>
+    <td><?php echo "$matKhau"; ?></td>
+    <td><?php echo "$tenHienThi"; ?></td>
+    <td><?php echo "$diaChi"; ?></td>
+    <td><?php echo "$dienThoai"; ?></td>
+    <td><?php echo "$email"; ?></td>
+    <td><?php echo "$maLoaiTaiKhoan"; ?></td>
+</tr>
