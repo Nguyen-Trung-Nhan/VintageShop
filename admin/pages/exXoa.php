d5b60a1d1139feb71885f46c11a22120b566d339
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-09 23:03:18 +0700

    Cap nhat xu ly xoa cho trang quan li nsx va nguoi dung

--- admin/pages/exXoa.php
+++ admin/pages/exXoa.php
@@ -5,10 +5,32 @@
         DataProvider::ExecuteQuery($sql);
         DataProvider::ChangeURL("index.php?a=2");
     }
+
     if(isset($_GET["b"]) && $_GET["b"] == 2 && isset($_POST["txtMaLoaiSanPham"])){
         $maLoaiSanPham = $_POST["txtMaLoaiSanPham"];
         $sql = "UPDATE loaisanpham SET BiXoa = 1 WHERE MaLoaiSanPham = '$maLoaiSanPham' ";
         DataProvider::ExecuteQuery($sql);
         DataProvider::ChangeURL("index.php?a=8");
+    }
+
+    if(isset($_GET["b"]) && $_GET["b"] == 3 && isset($_POST["txtMaHangSanXuat"])){
+        $maHangSanXuat = $_POST["txtMaHangSanXuat"];
+        $sql = "UPDATE hangsanxuat SET BiXoa = 1 WHERE MaHangSanXuat = '$maHangSanXuat' ";
+        DataProvider::ExecuteQuery($sql);
+        DataProvider::ChangeURL("index.php?a=11");
+    }
+    if(isset($_GET["b"]) && $_GET["b"] == 4 && isset($_POST["txtMaTaiKhoan"])){
+        $maTaiKhoan = $_POST["txtMaTaiKhoan"];
+        $sql = "UPDATE taikhoan SET BiXoa = 1 WHERE MaTaiKhoan = '$maTaiKhoan' ";
+        DataProvider::ExecuteQuery($sql);
+        DataProvider::ChangeURL("index.php?a=14");
+    }
+    if(isset($_GET["b"]) && $_GET["b"] == 5 && isset($_POST["txtMaDonDatHang"])){
+        $maDonDatHang = $_POST["txtMaDonDatHang"];
+        $sql = "DELETE FROM chitietdondathang WHERE  MaDonDatHang= '$maDonDatHang'";
+        DataProvider::ExecuteQuery($sql);
+        $sql = "DELETE FROM dondathang WHERE MaDonDatHang = '$maDonDatHang";
+        DataProvider::ExecuteQuery($sql);
+        DataProvider::ChangeURL("index.php?a=17");
     }
 ?>
