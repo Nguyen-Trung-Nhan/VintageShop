9fee66fedc0fbaa7b4e315a1e805879d4efeb49e
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-09 23:16:10 +0700

    Them templates nha san xuat

---
+++ admin/templates/tempLietKeHangSanXuat.php
@@ -0,0 +1,5 @@
+<tr>
+    <td><?php echo "$maHangSanXuat"; ?></td>
+    <td><?php echo "$tenHangSanXuat"; ?></td>
+    <td><img src="images/logo/<?php echo "$logoURL" ?> alt="k co hình"></td>
+</tr>
