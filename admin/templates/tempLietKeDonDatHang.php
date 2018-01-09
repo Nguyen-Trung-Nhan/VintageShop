51e321fc06fe5f1c3bf39f6dfe872bfd94f72eba
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-09 23:34:07 +0700

    Cap nhat templates liet ke don dat hang

---
+++ admin/templates/tempLietKeDonDatHang.php
@@ -0,0 +1,7 @@
+<tr>
+    <td><?php echo "$maDonDatHang"; ?></td>
+    <td><?php echo "$ngayLap"; ?></td>
+    <td><?php echo "$tongThanhTien"; ?></td>
+    <td><?php echo "$maTaiKhoan"; ?></td>
+    <td><?php echo "$maTinhTrang"; ?></td>
+</tr>
