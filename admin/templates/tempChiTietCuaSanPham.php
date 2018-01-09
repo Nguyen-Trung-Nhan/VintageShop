3c18d7b371ced29bc86a9c7520c0ce94a98a25af
NhonPham <1560397@student.hcmus.edu.vn>
2018-01-09 23:15:08 +0700

    Them templates chi tiet san pham

---
+++ admin/templates/tempChiTietCuaSanPham.php
@@ -0,0 +1,28 @@
+<div id="chitietsp">
+<div id="chitietleft">
+    <img src="images/<?php echo $hinhURL; ?>">
+</div>
+<div id="chitietright">
+    <div>
+        <span class="label">Tên sản phẩm:</span>
+        <span class="productname"><?php echo $tenSanPham; ?></span>
+    </div>
+    <div>
+        <span class="label">Giá:</span>
+        <span class="price"><?php echo $giaSanPham ?> đ</span>
+    </div>
+    <div>
+        <span class="label">Hãng sản xuất:</span>
+        <span class="factory"><?php echo $tenHangSanXuat; ?></span>
+    </div>
+    <div>
+        <span class="label">Loại sản phẩm:</span>
+        <span class="data"><?php echo $tenLoaiSanPham; ?></span>
+    </div>
+    <div>
+        <span class="label">Số lượng:</span>
+        <span class="data"><?php echo $soLuongTon; ?> sản phẩm</span>
+    </div>
+</div>
+<div id="mota"><?php echo $moTa; ?></div>
+</div>
